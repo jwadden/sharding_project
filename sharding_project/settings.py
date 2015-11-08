a8c4cc7bee78fdb8f2d40a51f57469463e109b64
Jeff Wadden <jwadden@eddie.missingcomma.com>
2015-11-08 12:21:59 -0500

    Additional settings for django-sharding.

--- sharding_project/settings.py
+++ sharding_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_sharding',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -78,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'SHARD_GROUP': None,
+        'PRIMARY': None,
     }
 }
 
